d03d188bffcfa09dc0556f8a6137b03637dd881b
jonathanku0313 <61576551+jonathanku0313@users.noreply.github.com>
2020-04-01 17:49:10 +0800

    exam01

--- main.cpp
+++ main.cpp
@@ -4,6 +4,12 @@
 #include "uLCD_4DGL.h"
 
 uLCD_4DGL uLCD(D1, D0, D2); // serial tx, serial rx, reset pin;
+Serial pc( USBTX, USBRX );
+float high = 0, low = 0.001;
+int sample = 128;
+float data[128];
+int Out;
+int n = 0;
 
 int main()
 {
@@ -13,4 +19,36 @@ int main()
     uLCD.text_height(4);
     uLCD.triangle(20, 20, 30, 30, 20, 30, RED);
     uLCD.triangle(21, 20, 30, 20, 30, 29, RED);
+
+
+    while(1){
+        for(float i = 0; i < 1; i += 0.1){
+            while(n < 10){
+              Out = 1;
+              wait(high);
+              Out = 0;
+              wait(low);
+            }
+            high += 0.0001;
+            low -= 0.0001;
+        }
+        for(float j = 1; j >= 0; j -= 0.1){
+            while(n < 10){
+              Out = 1;
+              wait(high);
+              Out = 0;
+              wait(low);
+            }
+            high -= 0.0001;
+            low += 0.0001;
+        }
+    }
+    for (int k = 0; k < sample; k++){
+      data[k] = Out;
+      wait(0.001/sample);
+    }
+    for (int m = 0; m < sample; m++){
+      pc.printf("%1.3f\r\n", data[m]);
+      wait(0.1);
+    }
 }
